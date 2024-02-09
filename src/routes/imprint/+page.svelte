1eded02f6818e03db5abbfdf8535f32c5d82aa52
Dennis Muensterer <dm@hpm.agency>
2024-02-09 22:50:04 +0100

    Add imprint page

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import H1 from "$lib/components/typography/H1.svelte";
+  import SvelteMarkdown from 'svelte-markdown'
+  import imprintText from "./imprint.md?raw";
+</script>
+
+<svelte:head>
+	<title>Imprint</title>
+</svelte:head>
+
+<div class="overflow-y-scroll">
+  <H1>Imprint</H1>
+  <SvelteMarkdown source={imprintText} renderers={{ h1: H1 }} />
+</div>
